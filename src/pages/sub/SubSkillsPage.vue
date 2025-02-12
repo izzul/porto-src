75c9b93692c3d912286868639f09fcce7a8809c3
aizzulf <afifdiy@gmail.com>
2025-02-13 06:51:19 +0700

    patch(page): add sub skill pages and register routes

---
+++ src/pages/sub/SubSkillsPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page padding>
+    <HeroSection/>
+    <h4 class="text-center text-weight-bolder">{{ skillName }}</h4>
+  </q-page>
+</template>
+
+<script setup>
+import HeroSection from 'components/HeroSection.vue'
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      skillName: '< My Skill >' 
+    }
+  },
+  mounted() {
+    this.skillName = '< ' + this.$route.name + ' >'
+  },
+  watch: {
+    $route(to) {
+      this.skillName = '< ' + to.name + ' >'
+      // Your code here, e.g. updating some data or fetching new data
+    }
+  }
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,18 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'about', component: () => import('pages/AboutPage.vue') },
+      { path: 'sub', component: () => import('pages/IndexPage.vue') }
+    ],
+  },
+  {
+    path: '/sub',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'fullstack', component: () => import('src/pages/sub/SubSkillsPage.vue'), name: 'Full-Stack' },
+      { path: 'devops', component: () => import('src/pages/sub/SubSkillsPage.vue'), name: 'Dev Ops' },
+      { path: 'data', component: () => import('src/pages/sub/SubSkillsPage.vue'), name: 'Data Engineer' },
+      { path: 'ai', component: () => import('src/pages/sub/SubSkillsPage.vue'), name: 'AI Engineer' }
     ]
   },
 
