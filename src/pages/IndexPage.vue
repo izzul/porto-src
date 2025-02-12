1bb8eebe2182db5da5a89747efcc82a224bae050
aizzulf <afifdiy@gmail.com>
2025-02-13 06:49:58 +0700

    patch(layout): make hero section as component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="text-center q-mx-lg">
-      <h2 class="q-my-lg">
-        Building <span class="text-weight-bolder">Scalable</span> Solutions
-      </h2>
-      <h2 class="q-my-lg">
-        with <span class="text-weight-bold">Data, Code, and AI</span>
-      </h2>
-      <h6 class="text-grey text-weight-regular q-my-lg">
-        I develop data-driven applications, optimize AI models, and build scalable SaaS solutions.
-      </h6>
-      <span>
-        <q-btn
-          no-caps
-          unelevated
-          color="black"
-          label="Backend + Frontend Dev"
-          class="q-my-md q-mx-sm q-py-sm"
-        />
-        <q-btn
-          no-caps
-          unelevated
-          color="black"
-          label="Dev Ops"
-          class="q-my-md q-mx-sm q-py-sm"
-        />
-        <q-btn
-          no-caps
-          unelevated
-          color="black"
-          label="Data Engineer"
-          class="q-my-md q-mx-sm q-py-sm"
-        />
-        <q-btn
-          no-caps
-          unelevated
-          color="black"
-          label="AI Engineer"
-          class="q-my-md q-mx-sm q-py-sm"
-        />
-      </span>
-    </div>
+    <HeroSection/>
   </q-page>
 </template>
 
 <script setup>
-//
+import HeroSection from 'components/HeroSection.vue'
+
 </script>
